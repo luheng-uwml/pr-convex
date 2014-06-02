6afa28cc46c47df326f56ed26077a533fa662692
luheng <luheng@uw.edu>
2014-06-02 02:40:11 -0700

    config

---
+++ src/experiment/ExperimentConfig.java
@@ -0,0 +1,35 @@
+package experiment;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+public class ExperimentConfig {
+	@Option(name = "-num-labels", usage="")
+	public int numLabeled = -1;
+	
+	@Option(name = "-graph", usage="")
+	public boolean useGraph;
+	
+	@Option(name = "-min-ff", usage="")
+	public int featureFreqCutOff = 1;
+	
+	@Option(name = "-lambda1", usage="")
+	public double lambda1 = 10;
+	
+	@Option(name = "-lambda2", usage="")
+	public double lambda2 = 0;
+	
+	@Option(name = "-matpath", usage="")
+	public String logFilePath = "./experiments/all_lambda1_01_lambda2_0.mat";
+	
+	public ExperimentConfig(String[] args) {
+		CmdLineParser parser = new CmdLineParser(this);
+		parser.setUsageWidth(120);
+		try {
+			parser.parseArgument(args);
+		} catch (CmdLineException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/experiment/RegularizedNERExperiment.java
+++ src/experiment/RegularizedNERExperiment.java
@@ -3,6 +3,8 @@ package experiment;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.kohsuke.args4j.CmdLineParser;
+
 import optimization.*;
 import data.Evaluator;
 import data.IOHelper;
@@ -14,7 +16,7 @@ import gnu.trove.list.array.TIntArrayList;
 
 public class RegularizedNERExperiment {
 	
-	private static void runNonRegularizedExperiment() {
+	private static void runRegularizedExperiment(ExperimentConfig config) {
 		NERCorpus corpusTrain = new NERCorpus();
 		corpusTrain.readFromCoNLL2003("./data/eng.train");
 	
@@ -29,7 +31,9 @@ public class RegularizedNERExperiment {
 		}
 		System.out.println("Number of all tokens:\t" + numAllTokens);
 		
-		int numTrains = numLabeled > 0 ? numLabeled : corpusTrain.size();
+		int numTrains = config.numLabeled > 0 ? config.numLabeled :
+			corpusTrain.size();
+		
 		int numInstances = numTrains + corpusDev.size();
 		ArrayList<NERSequence> trainInstances = new ArrayList<NERSequence>();
 		ArrayList<NERSequence> allInstances = new ArrayList<NERSequence>();
@@ -56,115 +60,47 @@ public class RegularizedNERExperiment {
 						   "\tnum all::\t" + numInstances);
 		
 		NERFeatureExtractor extractor = new NERFeatureExtractor(corpusTrain,
-				trainInstances, featureFreqCutOff);
+				trainInstances, config.featureFreqCutOff);
 		extractor.printInfo();
-		
-		SequentialFeatures features = extractor.
-				getSequentialFeatures(allInstances);
+		SequentialFeatures features = extractor.getSequentialFeatures(
+				allInstances);
 		Evaluator eval = new Evaluator(corpusTrain);
-		GraphRegularizer graph =
-				new DummyGraphRegularizer(features.numTargetStates);
-		double goldPenalty = graph.computeTotalPenalty(labels);
-		System.out.println("gold penalty::\t" + goldPenalty);
 		
-		// here lambda = 1 / C
-		RegularizedExponentiatedGradientDescent optimizer =
-				new RegularizedExponentiatedGradientDescent(features, graph,
-						labels, trainList.toArray(), devList.toArray(), eval,
-						lambda1, 0, 0.5, 500, 12345);
+		GraphRegularizer graph = new DummyGraphRegularizer(
+				features.numTargetStates);
 		
-		optimizer.optimize();
-		try {
-			IOHelper.saveOptimizationHistory(optimizer.history, logFilePath);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private static void runRegularizedExperiment() {
-		NERCorpus corpusTrain = new NERCorpus();
-		corpusTrain.readFromCoNLL2003("./data/eng.train");
-	
-		NERCorpus corpusDev = new NERCorpus(corpusTrain, false);
-		corpusDev.readFromCoNLL2003("./data/eng.testa");
-		corpusTrain.printCorpusInfo();
-		corpusDev.printCorpusInfo();
-		
-		int numAllTokens = 0;
-		for (NERSequence instance : corpusTrain.instances) {
-			numAllTokens += instance.length;
-		}
-		System.out.println("Number of all tokens:\t" + numAllTokens);
-		
-		int numTrains = numLabeled > 0 ? numLabeled : corpusTrain.size();
-		int numInstances = numTrains + corpusDev.size();
-		ArrayList<NERSequence> trainInstances = new ArrayList<NERSequence>();
-		ArrayList<NERSequence> allInstances = new ArrayList<NERSequence>();
-		TIntArrayList trainList = new TIntArrayList(),
-					  devList = new TIntArrayList();
-		int[][] labels = new int[numInstances][];
-		// add from original TRAIN corpus
-		for (int i = 0; i < Math.min(numTrains, corpusTrain.size()); i++) {
-			int instanceID = allInstances.size();
-			allInstances.add(corpusTrain.instances.get(i));
-			labels[instanceID] = corpusTrain.instances.get(i).getLabels();
-			trainInstances.add(corpusTrain.instances.get(i));
-			trainList.add(instanceID);
-		}
-		// add from original DEV corpus
-		for (int i = 0; i < Math.min(numInstances - numTrains,
-				corpusDev.size()); i++) {
-			int instanceID = allInstances.size();
-			allInstances.add(corpusDev.instances.get(i));
-			labels[instanceID] = corpusDev.instances.get(i).getLabels();
-			devList.add(instanceID);
-		}
-		System.out.println("num trains::\t" + numTrains +
-						   "\tnum all::\t" + numInstances);
-		
-		NERFeatureExtractor extractor = new NERFeatureExtractor(corpusTrain,
-				trainInstances, featureFreqCutOff);
-		extractor.printInfo();
-		
-		NGramFeatureExtractor ngramExtractor = new NGramFeatureExtractor(
-				corpusTrain, allInstances);
-		KNNGraphConstructor graphConstructor = new KNNGraphConstructor(
-				ngramExtractor.getNGramFeatures(), 10, true, 0.3, 8);
-		graphConstructor.run();
-		
-		SequentialFeatures features = extractor.
-				getSequentialFeatures(allInstances);
-		Evaluator eval = new Evaluator(corpusTrain);
-		GraphRegularizer graph =
-				new GraphRegularizer(ngramExtractor.getNGramIDs(),
+		if (config.useGraph) {
+			NGramFeatureExtractor ngramExtractor = new NGramFeatureExtractor(
+					corpusTrain, allInstances);
+			KNNGraphConstructor graphConstructor = new KNNGraphConstructor(
+					ngramExtractor.getNGramFeatures(), 10, true, 0.3, 8);
+			graphConstructor.run();
+			graph = new GraphRegularizer(ngramExtractor.getNGramIDs(),
 					graphConstructor.getEdgeList(), features.numTargetStates);
-				//new DummyGraphRegularizer(features.numTargetStates);
-		
-		double goldPenalty = graph.computeTotalPenalty(labels);
-		System.out.println("gold penalty::\t" + goldPenalty);
-		
-		graph.validate(labels, corpusTrain.nerDict, ngramExtractor.ngramDict);
-		
+			double goldPenalty = graph.computeTotalPenalty(labels);
+			System.out.println("gold penalty::\t" + goldPenalty);
+			graph.validate(labels, corpusTrain.nerDict,
+					ngramExtractor.ngramDict);
+		}
+			
 		// here lambda = 1 / C
 		RegularizedExponentiatedGradientDescent optimizer =
 				new RegularizedExponentiatedGradientDescent(features, graph,
 						labels, trainList.toArray(), devList.toArray(), eval,
-						lambda1, lambda2, 0.5, 500, 12345);
+						config.lambda1, config.useGraph ? config.lambda2 : 0,
+						0.5, 500, 12345);
 		
 		optimizer.optimize();
 		try {
-			IOHelper.saveOptimizationHistory(optimizer.history, logFilePath);
+			IOHelper.saveOptimizationHistory(optimizer.history,
+					config.logFilePath);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private static int numLabeled = -1;
-	private static int featureFreqCutOff = 1;
-	private static double lambda1 = 10, lambda2 = 0;
-	private static String logFilePath = "./experiments/all_lambda1_01_lambda2_0.mat";
 	
 	public static void main(String[] args) {
-		runNonRegularizedExperiment();
+		ExperimentConfig config = new ExperimentConfig(args);
+		runRegularizedExperiment(config);
 	}
 }
